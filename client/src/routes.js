497d11ed9f9e68036e45fecb34b1a9fda7509a1a
Akataya <infotechpro@yahoo.com>
2019-05-23 23:17:27 -0400

    Added login/register functionality

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,12 +3,20 @@ import { Switch, Route } from "react-router-dom";
 
 import Layout from "./Hoc/Layout";
 import Home from "./components/Home";
+import RegisterLogin from "./components/Register_login";
+import Register from "./components/Register_login/register";
+
+import UserDashboard from "./components/User";
 
 const Routes = ({}) => {
   return (
     <Layout>
       <Switch>
-        <Route path="" exact component={Home} />
+        <Route path="/user/dashboard" exact component={UserDashboard} />
+
+        <Route path="/register" exact component={Register} />
+        <Route path="/register_login" exact component={RegisterLogin} />
+        <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
   );
